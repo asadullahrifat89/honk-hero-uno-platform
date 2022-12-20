11d1edb7eb992ccb301eb0abf27d6c96a0a69b42
Asadullah Rifat <asadullah.rifat@selise.ch>
2022-12-20 13:49:29 +0600

    wasm solution fixed | game play page renamed

--- src/HonkHeroGame/HonkHeroGame.Shared/App.xaml.cs
+++ src/HonkHeroGame/HonkHeroGame.Shared/App.xaml.cs
@@ -80,7 +80,7 @@ namespace HonkHeroGame
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage), args.Arguments);
+                    rootFrame.Navigate(typeof(GamePlayPage), args.Arguments);
                 }
                 // Ensure the current window is active
                 _window.Activate();
--- src/HonkHeroGame/HonkHeroGame.Shared/GamePlayPage.xaml.cs
+++ src/HonkHeroGame/HonkHeroGame.Shared/GamePlayPage.xaml.cs
@@ -13,16 +13,11 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
 namespace HonkHeroGame
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class GamePlayPage : Page
     {
-        public MainPage()
+        public GamePlayPage()
         {
             this.InitializeComponent();
         }
