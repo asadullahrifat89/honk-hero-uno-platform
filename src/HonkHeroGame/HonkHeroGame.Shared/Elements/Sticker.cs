2ee7372a92e30ae65c7287d2cc20422c2c86ee92
Asadullah Rifat <asadullah.rifat@selise.ch>
2022-12-29 23:58:05 +0600

    game lag prevented with collectible and sticker borders omitting

--- src/HonkHeroGame/HonkHeroGame.Shared/Elements/Sticker.cs
+++ src/HonkHeroGame/HonkHeroGame.Shared/Elements/Sticker.cs
@@ -15,9 +15,9 @@ namespace HonkHeroGame
 
             SetContent(Constants.ELEMENT_TEMPLATES.FirstOrDefault(x => x.Key is ElementType.STICKER).Value);
 
-            CornerRadius = new Microsoft.UI.Xaml.CornerRadius(5);
-            BorderBrush = new SolidColorBrush(Colors.Black);
-            BorderThickness = new Microsoft.UI.Xaml.Thickness(2);
+            //CornerRadius = new Microsoft.UI.Xaml.CornerRadius(5);
+            //BorderBrush = new SolidColorBrush(Colors.Black);
+            //BorderThickness = new Microsoft.UI.Xaml.Thickness(2);
         }
     }
 }
